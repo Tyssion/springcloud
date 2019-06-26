f385329a27516a3e6e63aadd4b9ee9419e4d256d
chingzhang <18749689868@163.com>
2019-06-26 15:24:51 +0800

    Zuul网关组件使用

--- producer/src/main/java/com/qing/controller/UserController.java
+++ producer/src/main/java/com/qing/controller/UserController.java
@@ -3,6 +3,7 @@ package com.qing.controller;
 import com.qing.entity.User;
 import com.qing.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,14 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/getPort")
+    public String getPort(){
+        return "当前端口号：" + this.port;
+    }
+
     @GetMapping("/user")
     public List<User> getUser(){
        return userService.getUser();
@@ -28,4 +37,6 @@ public class UserController {
     public User getUserById(@PathVariable("id") int id){
         return userService.getUserById(id);
     }
+
+
 }
