a5fee385b3b1eb8ed2143ab5b5237efdc819d2e3
chingzhang <18749689868@163.com>
2019-06-27 14:43:34 +0800

    基于Feign实现负载均衡

---
+++ feign/src/main/java/com/qing/FeignApplication.java
@@ -0,0 +1,18 @@
+package com.qing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @Author: zhching
+ * @Date: 2019/6/27 13:57
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class FeignApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FeignApplication.class,args);
+    }
+}
---
+++ feign/src/main/java/com/qing/feign/FeignProducerClient.java
@@ -0,0 +1,22 @@
+package com.qing.feign;
+
+import com.qing.entity.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+/**
+ * @Author: zhching
+ * @Date: 2019/6/27 14:04
+ * @Version 1.0
+ */
+@FeignClient(value = "producer-server")
+public interface FeignProducerClient {
+    @GetMapping("/user")
+    List<User> getUser();
+
+    @GetMapping("/getPort")
+    String getPort();
+}
